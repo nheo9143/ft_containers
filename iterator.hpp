0fcbfde39334bfb59a094c5434917b657745246e
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-01-23 21:05:58 +0900

    [feat] random access iterator

--- iterator.hpp
+++ iterator.hpp
@@ -12,8 +12,8 @@ struct forward_iterator_tag : input_iterator_tag {};
 struct bidirectional_iterator_tag : forward_iterator_tag {};
 struct random_access_iterator_tag : bidirectional_iterator_tag {};
 
-template<class _Category, class _Tp, class _Distance = ptrdiff_t,
-         class _Pointer = _Tp*, class _Reference = _Tp&>
+template<typename _Category, typename _Tp, typename _Distance = ptrdiff_t,
+         typename _Pointer = _Tp*, typename _Reference = _Tp&>
 struct iterator
 {
     typedef _Tp        value_type;
@@ -24,10 +24,128 @@ struct iterator
 };
 
 template <typename Iterator>
-class iterator
+class random_access_iterator
 {
-	typedef Iterator iterator_type;
+    public:
+        typedef Iterator iterator_type;
+        typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
+        typedef typename iterator_traits<Iterator>::value_type        value_type;
+        typedef typename iterator_traits<Iterator>::difference_type   difference_type;
+        typedef typename iterator_traits<Iterator>::pointer           pointer;
+        typedef typename iterator_traits<Iterator>::reference         reference;
+
+    private:
+        iterator_type _current;
+
+    public:
+        template <typename Iter>
+        random_access_iterator() : _current() {}
+        random_access_iterator(const random_access_iterator<Iter>& other) : _current(other.base()) {}
+        iterator(random_access_iterator it) : _current(it) {}
+        iterator(const random_access_iterator& other) : _current(other._it) {}
+
+        random_access_iterator& operator=(const random_access_iterator& other)
+        {
+            _current = other._current;
+            return *this;
+        }
+
+        ~random_access_iterator() {}
+
+        const iterator_type base() const { return _current; }
+
+        reference operator*() const { return *_current; }
+
+        pointer operator->() const { return _current; }
+
+        random_access_iterator& operator++()
+        {
+            ++_current;
+            return *this;
+        }
+
+        random_access_iterator operator++(int)
+        {
+            random_access_iterator tmp(*this);
+            ++_current;
+            return tmp;
+        }
+
+        random_access_iterator& operator--()
+        {
+            --_current;
+            return *this;
+        }
+
+        random_access_iterator operator--(int)
+        {
+            iterator tmp(*this);
+            --_current;
+            return tmp;
+        }
+
+        random_access_iterator operator+(difference_type n) const
+        {
+            return random_access_iterator(_current + n);
+        }
+
+        random_access_iterator operator-(difference_type n) const
+        {
+            return random_access_iterator(_current - n);
+        }
+
+        random_access_iterator& operator+=(difference_type n)
+        {
+            _current += n;
+            return *this;
+        }
+
+        random_access_iterator& operator-=(difference_type n)
+        {
+            _current -= n;
+            return *this;
+        }
+
+        reference operator[](difference_type n) const
+        {
+            return _current[n];
+        }
 };
+
+template <typename Iterator>
+bool operator==(const random_access_iterator<Iterator>& lhs, const random_access_iterator<Iterator>& rhs)
+{
+    return lhs.base() == rhs.base();
+}
+
+template <typename Iterator>
+bool operator!=(const random_access_iterator<Iterator>& lhs, const random_access_iterator<Iterator>& rhs)
+{
+    return lhs.base() != rhs.base();
+}
+
+template <typename Iterator>
+bool operator<(const random_access_iterator<Iterator>& lhs, const random_access_iterator<Iterator>& rhs)
+{
+    return lhs.base() < rhs.base();
+}
+
+template <typename Iterator>
+bool operator<=(const random_access_iterator<Iterator>& lhs, const random_access_iterator<Iterator>& rhs)
+{
+    return lhs.base() <= rhs.base();
+}
+
+template <typename Iterator>
+bool operator>(const random_access_iterator<Iterator>& lhs, const random_access_iterator<Iterator>& rhs)
+{
+    return lhs.base() > rhs.base();
+}
+
+template <typename Iterator>
+bool operator>=(const random_access_iterator<Iterator>& lhs, const random_access_iterator<Iterator>& rhs)
+{
+    return lhs.base() >= rhs.base();
 }
 
 #endif
