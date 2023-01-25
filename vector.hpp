a7c6148ba8665f3a8d3e053ec112930184d16b6a
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-01-25 17:06:47 +0900

    [refactor] 변수명 줄맞춤

--- vector.hpp
+++ vector.hpp
@@ -10,25 +10,26 @@ namespace ft
 	class vector
 	{
 		public:
-			typedef T value_type;
-			typedef Alloc allocator_type;
-			typedef typename allocator_type::reference reference;
-			typedef typename allocator_type::const_reference const_reference;
-			typedef typename allocator_type::pointer pointer;
-			typedef typename allocator_type::const_pointer const_pointer;
-			typedef typename allocator_type::difference_type difference_type;
-			typedef typename allocator_type::size_type size_type;
+			typedef T 		value_type;
+			typedef Alloc	allocator_type;
 
-			typedef typename ft::random_access_iterator<pointer> iterator;
-			typedef typename ft::random_access_iterator<const_pointer> const_iterator;
-			typedef typename ft::reverse_iterator<iterator> reverse_iterator;
-			typedef typename ft::reverse_iterator<const_iterator> const_reverse_iterator;
+			typedef typename allocator_type::reference			reference;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
+			typedef typename allocator_type::const_pointer		const_pointer;
+			typedef typename allocator_type::difference_type	difference_type;
+			typedef typename allocator_type::size_type			size_type;
 
-		protected:
-			pointer _begin;
-			pointer _end;
-			pointer _end_cap;
-			allocator_type _a;
+			typedef typename ft::random_access_iterator<pointer>		iterator;
+			typedef typename ft::random_access_iterator<const_pointer>	const_iterator;
+			typedef typename ft::reverse_iterator<iterator>				reverse_iterator;
+			typedef typename ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+
+		private:
+			pointer			_begin;
+			pointer			_end;
+			pointer			_end_cap;
+			allocator_type	_a;
 
 		public:
 			/* constructer */
