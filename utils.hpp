3914d68ccb7f4e8cead1512752f077a830130484
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-01-25 16:34:10 +0900

    [refactor] enable_if, is_integral을 util로 이동

--- utils.hpp
+++ utils.hpp
@@ -28,6 +28,54 @@ namespace ft
 		}
 		return (first2!=last2);
 	}
+
+	/* enable_if 기본형 선언 */
+	template <bool, typename T>
+	struct enable_if {};
+
+	/* true가 들어왔을 때의 처리 */
+	template <typename T>
+	struct enable_if<true, T>
+	{
+		typedef T type;
+	};
+
+	/* is_integral 기본형 선언 */
+	template<typename T>
+	struct is_integral { static const bool value = false;};
+
+	template<>
+	struct is_integral<bool> {static const bool value = true;};
+
+	template<>
+	struct is_integral<char> {static const bool value = true;};
+
+	template<>
+	struct is_integral<signed char> {static const bool value = true;};
+
+	template<>
+	struct is_integral<wchar_t> {static const bool value = true;};
+
+	template<>
+	struct is_integral<short> {static const bool value = true;};
+
+	template<>
+	struct is_integral<int> {static const bool value = true;};
+
+	template<>
+	struct is_integral<long> {static const bool value = true;};
+
+	template<>
+	struct is_integral<unsigned char> {static const bool value = true;};
+
+	template<>
+	struct is_integral<unsigned short> {static const bool value = true;};
+
+	template<>
+	struct is_integral<unsigned int> {static const bool value = true;};
+
+	template<>
+	struct is_integral<unsigned long> {static const bool value = true;};
 }
 
 #endif
