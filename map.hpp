ed0ce3f531991c64ceb32d0300cc34ac6ee5fe72
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-01-26 13:24:22 +0900

    [feat] ft::pair 추가

--- map.hpp
+++ map.hpp
@@ -2,17 +2,18 @@
 # define MAP_HPP
 
 # include "utils.hpp"
+# include "pair.hpp"
 # include <map>
 
 namespace ft
 {
-	template <typename Key, typename T, typename Compare = std::less<Key>, typename Alloc = std::allocator<std::pair<const Key, T> > >
+	template <typename Key, typename T, typename Compare = std::less<Key>, typename Alloc = std::allocator<ft::pair<const Key, T> > >
 	class map
 	{
 		public:
 			typedef Key											key_type;
 			typedef T											mapped_type;
-			typedef std::pair<const key_type, mapped_type>		value_type;
+			typedef ft::pair<const key_type, mapped_type>		value_type;
 			typedef Compare										key_compare;
 			typedef Alloc										allocator_type;
 
