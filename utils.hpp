b3f27d992af7a39ae7e12a2e5d6d17b175694c0b
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-02-08 20:23:45 +0900

    [refactor] 띄워쓰기 한 칸 추가

--- utils.hpp
+++ utils.hpp
@@ -14,7 +14,7 @@ namespace ft
 	};
 
 	template <typename T>
-	struct less : public binary_function<T, T, bool>{
+	struct less : public binary_function<T, T, bool> {
 		bool operator() (const T& x, const T& y) const {return x < y;}
 	};
 
