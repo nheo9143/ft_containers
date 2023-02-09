2280c3ebc26c9ac61e2de359e760dd4652ad35f2
Heo Namjun <heonamjun@Heos-MacBook-Air.local>
2023-02-09 11:12:06 +0900

    [refactor] node 파일 분리

--- tree.hpp
+++ tree.hpp
@@ -5,60 +5,10 @@
 # include "pair.hpp"
 # include "tree_iterator.hpp"
 # include "iterator.hpp"
-
-# define RED		true
-# define BLACK		false
+# include "node.hpp"
 
 namespace ft
 {
-	template <typename T>
-	class node {
-		public:
-
-			T			_value;
-			node*		_parent;
-			node*		_left;
-			node*		_right;
-			bool		_color;
-
-			node(): _value(), _parent(0), _left(0), _right(0), _color(RED) {}
-
-			node(const T& value, node* parent = 0, node* left = 0, node* right = 0, bool color = RED) : _value(value), _parent(parent), _left(left), _right(right), _color(color) {}
-
-			node(const node& ref) : _value(ref._value), _parent(ref._parent), _left(ref._left), _right(ref._right), _color(ref._color) {}
-			
-			virtual ~node() {}
-			
-			node&	operator=(const node& ref) {
-				if (this != &ref) {
-					this->_value = ref._value;
-					this->_parent = ref._parent;
-					this->_left = ref._left;
-					this->_right = ref._right;
-					this->_color = ref._color;
-				}
-				return (*this);
-			}
-
-			bool	operator==(const node& ref) const {
-				return (this->_value == ref._value);
-			}
-
-			bool	operator!=(const node& ref) const {
-				return (this->_value != ref._value);
-			}
-	};
-
-	template <class T1, class T2>
-	bool operator==(const node<T1>& lhs, const node<T2>& rhs) {
-		return lhs == rhs;
-	}
-	
-	template <class T1, class T2>
-	bool operator!=(const node<T1>& lhs, const node<T2>& rhs) {
-		return lhs != rhs;
-	}
-
 	template <typename T, typename Compare, typename Alloc = std::allocator<T> >
 	class tree
 	{
@@ -78,10 +28,10 @@ namespace ft
 			typedef typename allocator_type::const_reference	const_reference;
 			typedef size_t		size_type;
 			typedef ptrdiff_t	difference_type;
-			// typedef tree_iterator<T>	iterator;
-			// typedef tree_iterator<const T>	const_iterator;
-			// typedef tree_reverse_iterator<iterator>	reverse_iterator;
-			// typedef tree_reverse_iterator<const_iterator>	const_reverse_iterator;
+			typedef tree_iterator<T>	iterator;
+			typedef tree_iterator<const T>	const_iterator;
+			typedef reverse_iterator<iterator>	reverse_iterator;
+			// typedef reverse_iterator<const_iterator>	const_reverse_iterator;
 
 		private:
 			node_ptr		_root;
